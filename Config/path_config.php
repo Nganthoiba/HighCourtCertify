849aac02fe455447a20a3ffc8c41b766ba88d185
Nganthoiba <Nganthoiba@DESKTOP-CLD3BN8>
2020-01-13 11:39:30 +0530

    path configuration, setting upload path.

---
+++ Config/path_config.php
@@ -0,0 +1,11 @@
+<?php
+/********* Path Configurations ********/
+define('DS', '/');
+define('ROOT', '../');
+define('VIEWS_PATH', ROOT.'Views');
+define('LIBS_PATH', ROOT.'libs');
+define('INCLUDES_PATH', ROOT.'Includes');
+define('CONFIG_PATH', ROOT.'Config');
+define('UPLOAD_PATH', ROOT.'Uploads');
+/***** End of Path Configurations *****/
+
