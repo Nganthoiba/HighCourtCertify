f5646bf8ab1a7d5bb22d82f504e39941aefab7ec
Nganthoiba <Nganthoiba@DESKTOP-CLD3BN8>
2020-01-13 11:44:20 +0530

    Introducing case body concept

---
+++ Controllers/CasebodyController.php
@@ -0,0 +1,122 @@
+<?php
+/**
+ * Description of CasebodyController
+ *
+ * @author Nganthoiba
+ */
+class CasebodyController extends Controller{
+    public function __construct($data = array ()) {
+        parent::__construct($data);
+        $this->response = new Response();
+    }
+    public function uploadCaseBody(){
+        if(!Logins::isAuthenticated()){
+            $this->response->set([
+                "msg"=>"Please login and try again.",
+                "status_code"=>400
+            ]);
+            return $this->sendResponse($this->response);
+        }
+        
+        $input_data = $this->_cleanInputs($_POST);
+        if(sizeof($input_data) === 0){
+            $this->response->set([
+                "msg"=>"No input data",
+                "status_code"=>400
+            ]);
+            return $this->sendResponse($this->response);
+        }
+        
+        $this->response = verifyCSRFToken();
+        if(!$this->response->status){
+            return $this->sendResponse($this->response);
+        }
+        //$application_id = $input_data['application_id'];
+
+        //input validation for case body data
+        $this->response = $this->validateCasebody($input_data);
+        if(!$this->response->status){
+            return $this->sendResponse($this->response);
+        }
+        
+        $upload_directory = UPLOAD_PATH."/documents/case_body/";
+        $file_upload = new Upload();
+        $file_upload->setFileUploadKeyName("case_body_file");
+        $this->response = $file_upload->uploadSingleFile($upload_directory);
+        if(!$this->response->status){
+            return $this->sendResponse($this->response);
+        }
+        
+        $user_info = $_SESSION['user_info'];
+        $user_id = $user_info['user_id'];
+        
+        $casebody = new Casebody();
+        $casebody->case_number = $input_data['case_number'];
+        $casebody->case_year = $input_data['case_year'];
+        $casebody->case_type = $input_data['case_type'];
+        $casebody->document_path = $this->response->data['file_paths'][0];
+        $casebody->created_by = $user_id;
+        
+        $this->response = $casebody->add();
+        if($this->response->status){
+            $this->response->msg = "Case body has been created.";
+        }
+        return $this->sendResponse($this->response);
+    }
+    //load file for viewing
+    public function displayFile(){
+        $params = $this->getParams();
+        if(sizeof($params)){
+            $casebody_id = $params[0];
+            $casebody = new Casebody();
+            $casebody = $casebody->find($casebody_id);
+            if($casebody!=null){
+                //$casebody->loadFile();
+                downloadFile($casebody->document_path,false);
+            }
+        }
+    }
+    
+    //method to download case body file
+    public function downloadFile(){
+        $params = $this->getParams();
+        if(sizeof($params)){
+            $casebody_id = $params[0];
+            $casebody = new Casebody();
+            $casebody = $casebody->find($casebody_id);
+            if($casebody!=null){
+                downloadFile($casebody->document_path,true);
+            }
+        }
+    }
+    
+    //function to validate case body
+    private function validateCasebody($input_data = array()){
+        if(!isset($input_data['case_type']) || !isset($input_data['case_number']) || !isset($input_data['case_year'])){
+            $this->response->set([
+                "status"=>false,
+                "status_code"=>400,
+                "msg"=>"Missing input perameters"
+            ]);
+        }
+        else{
+            $casebody = new Casebody();
+            $res = $casebody->isCaseBodyExist($input_data['case_type'], $input_data['case_number'], $input_data['case_year']);
+            if($res){
+                $this->response->set([
+                    "status"=>false,
+                    "status_code"=>403,
+                    "msg"=>"Case body already exist."
+                ]);
+            }
+            else{
+                $this->response->set([
+                    "status"=>true,
+                    "status_code"=>200,
+                    "msg"=>"validated"
+                ]);
+            }
+        }
+        return $this->response;
+    }
+}
---
+++ Models/Casebody.php
@@ -0,0 +1,95 @@
+<?php
+/**
+ * Description of Casebody
+ *
+ * @author Nganthoiba
+ */
+class Casebody extends model{
+    /*
+    casebody_id   serial NOT NULL,
+    case_type      integer,
+    case_number    integer,
+    case_year      integer,
+    document_path  varchar(255) NOT NULL,
+    create_at      timestamp with time zone,
+    created_by     varchar(40),
+    */   
+    public  $casebody_id,
+            $case_type,
+            $case_number,
+            $case_year,
+            $document_path,
+            $create_at,
+            $created_by;
+    public function __construct() {
+        parent::__construct();
+        $this->setTable("casebody");
+        $this->setKey("casebody_id");
+        $this->response = new Response();
+    }
+    
+    public function add(){
+        $this->casebody_id = $this->findMaxColumnValue("casebody_id")+1;
+        $this->create_at = date('Y-m-d H:i:s');
+        if($this->isCaseBodyExist($this->case_type,$this->case_number,$this->case_year)){
+            $this->response->set([
+                "status"=>false,
+                "status_code"=>403,
+                "msg" => "Case body already exist"
+            ]);
+            return $this->response;
+        }
+        $rec = [
+            "casebody_id"=>$this->casebody_id,
+            "case_type" => $this->case_type,
+            "case_number" => $this->case_number,
+            "case_year" => $this->case_year,
+            "document_path" => $this->document_path,
+            "create_at" => $this->create_at,
+            "created_by" => $this->created_by
+        ];
+        return parent::create($rec);
+    }
+    
+    public function isCaseBodyExist($case_type,$case_number,$case_year){
+        $cond = [
+            "case_type" => $case_type,
+            "case_number" => $case_number,
+            "case_year" => $case_year
+        ];
+        $res = $this->read([], $cond);
+        return $res->status;
+    }
+    
+    //get case body according to case type, case number and case year
+    public function getCaseBody($case_type,$case_number,$case_year){
+        $cond = [
+            "case_type" => $case_type,
+            "case_number" => $case_number,
+            "case_year" => $case_year
+        ];
+        $this->response = $this->read([], $cond);
+        if($this->response->status){
+            return $this->response->data[0];
+        }
+        return null;
+    }
+    //file reading and writing functions
+    public function loadFile(){
+        if(file_exists($this->document_path)) {
+            header('Content-Description: File Transfer');
+            //header('Content-Type: application/octet-stream');
+            header('Content-Type: application/pdf');
+            //header('Content-Disposition: attachment; filename="'.basename($this->document_path).'"');
+            header('Expires: 0');
+            header('Cache-Control: must-revalidate');
+            header('Pragma: public');
+            header('Content-Length: ' . filesize($this->document_path));
+            flush(); // Flush system output buffer
+            readfile($this->document_path);
+        } else {
+            http_response_code(404);            
+        }
+        die();
+    }
+}
