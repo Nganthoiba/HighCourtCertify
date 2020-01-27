4fa48a65f332da667a058ff56d7d86ad4628a9ba
Nganthoiba <Nganthoiba@DESKTOP-CLD3BN8>
2020-01-27 23:06:20 +0530

    changing path configurations

--- libs/init.php
+++ libs/init.php
@@ -1,13 +1,15 @@
 <?php
-require_once ROOT.DS.'Config'.DS.'config.class.php';
-require_once ROOT.DS.'Config'.DS.'config.php';
-require_once ROOT.DS.'libs'.DS.'special_functions.php';
+require_once CONFIG_PATH.DS.'config.class.php';
+require_once CONFIG_PATH.DS.'config.php';
+require_once LIBS_PATH.DS.'special_functions.php';
 
 try{
     spl_autoload_register(function($classname) {
         $lib_path = ROOT.DS.'libs'.DS.strtolower($classname).'.class.php';
-        $controller_path = ROOT.DS.'Controllers'.DS. str_replace('controller','',strtolower($classname)).'Controller.php';
-        $model_path = ROOT.DS.'Models'.DS.($classname).'.php';
+        $controller_path = CONTROLLERS_PATH.DS. str_replace('controller','',strtolower($classname)).'Controller.php';
+        $model_path = MODELS_PATH.DS.($classname).'.php';
+        $entity_path = ENTITY_PATH.DS.($classname).'.php';
+        
         if(file_exists($lib_path)){
             require_once ($lib_path);
         }
@@ -17,11 +19,16 @@ try{
         if(file_exists($model_path)){
             require_once ($model_path);
         }
-        if(!file_exists($lib_path) && !file_exists($controller_path) && !file_exists($model_path)){
+        if(file_exists($entity_path)){
+            require_once ($entity_path);
+        }
+        
+        if(!file_exists($lib_path) && !file_exists($controller_path) && !file_exists($model_path) && !file_exists($entity_path)){
             throw new Exception("Class : ".$classname." does not exist.",404);
         }
+        
     });
 }catch(Exception $e){
-    echo $e->getMessage();
+    throw $e;
 }
 
